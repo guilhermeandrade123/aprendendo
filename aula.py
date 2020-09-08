6c92426491f9a148ab5a86277a7e87ceacd6c7fd
guilherme lima <guilherme.andrade11@fatec.sp.gov.br>
2020-09-08 10:50:09 -0300

    ja faz soma e subtraçao

---
+++ aula.py
@@ -0,0 +1,14 @@
+print ("Soma")
+
+op = int (input ("1-soma 2-subtracao"))
+a = int (input ("Primeiro operando"))
+
+b = int (input ("Segundo operando"))
+
+if op==1:
+   res = a + b
+
+else:
+   res = a - b
+
+print (res)
